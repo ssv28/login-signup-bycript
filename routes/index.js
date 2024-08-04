161a26ef3ffef618ada9311f66bef7d5d9490f1f
Your Name <you@example.com>
2024-08-04 22:14:31 +0530

    Add 4/8

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,69 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 
 const saltRounds = 10;
-let users = []; 
+let users = [];    // In-memory store for users
 
+// Middleware to parse request bodies
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/signup', async function (req, res, next) {
-  const { username, password } = req.body;
-
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(password, salt);
-
-  users.push({ username, password: hash });
-
-  res.redirect('/login');
-});
-
-
-router.get('/signup', function (req, res, next) {
+/* signup page. */
+router.get('/', function (req, res, next) {
   res.render('signup', { title: 'Signup' });
 });
 
+
+/* login page. */
 router.get('/login', function (req, res, next) {
   res.render('index', { title: 'Log in' });
 });
 
 
+
+/* signup data. */
+router.post('/', async function (req, res, next) {
+  
+  const { username, password } = req.body;
+  console.log(req.body)
+
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
+
+  console.log(">>>",salt)
+  console.log(hash)
+
+  users.push({ username, password: hash });
+
+  res.redirect('/login');
+
+});
+
+
+/* POST login data. */
+
+router.post('/login', async function (req, res) {
+  
+  const { username, password } = req.body;
+  console.log(req.body)
+
+  const user = users.find(user => user.username === username);
+  console.log(user)
+
+  if (!user) {
+    return res.send('User not found.');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  console.log(match)
+
+  if (match) {
+    res.send('Login successful!');
+  } else {
+    res.send('Incorrect password.');
+  }
+
+});
+
 module.exports = router;
-
-
-
-
 
 
 
